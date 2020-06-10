2212d941867ca8022b199a86018beb96d12fa829
Thusa <thusa.arul@hotmail.com>
2020-06-10 16:58:10 -0400

    added most of js, created ajax calls

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,489 @@
-// on button start run init
+const locales = [
+    {
+        city: 'Addis Ababa',
+        country: 'Ethiopia'
+    },
+    {
+        city: 'Agra',
+        country: 'India'
+    },
+    {
+        city: 'Amsterdam',
+        country: 'The Netherlands'
+    },
+    {
+        city: 'Anchorage',
+        country: 'Alaska'
+    },
+    {
+        city: 'Angkor Wat',
+        country: 'Cambodia'
+    },
+    {
+        city: 'Athens',
+        country: 'Greece'
+    },
+    {
+        city: 'Atlantic City',
+        country: 'New Jersey'
+    },
+    {
+        city: 'Auckland',
+        country: 'New Zealand'
+    },
+    {
+        city: 'Bali',
+        country: 'Indonesia'
+    },
+    {
+        city: 'Bangkok',
+        country: 'Thailand'
+    },
+    {
+        city: 'Barcelona',
+        country: 'Spain'
+    },
+    {
+        city: 'Bay Lake',
+        country: 'Florida'
+    },
+    {
+        city: 'Beijing',
+        country: 'China'
+    },
+    {
+        city: 'Berlin',
+        country: 'Germany'
+    },
+    {
+        city: 'Hamilton',
+        country: 'Burmuda'
+    },
+    {
+        city: 'Bethlehem',
+        country: 'Pennsylvania'
+    },
+    {
+        city: 'Bogota',
+        country: 'Columbia'
+    },
+    {
+        city: 'Papeete',
+        country: 'Bora Bora'
+    },
+    {
+        city: 'Brussels',
+        country: 'Belgium'
+    },
+    {
+        city: 'Budapest',
+        country: 'Hungary'
+    },
+    {
+        city: 'Buenos Aires',
+        country: 'Argentina'
+    },
+    {
+        city: 'Cairo',
+        country: 'Egypt'
+    },
+    {
+        city: 'Calgary',
+        country: 'Canada'
+    },
+    {
+        city: 'Cancun',
+        country: 'Mexico'
+    },
+    {
+        city: 'Cape Town',
+        country: 'South Africa'
+    },
+    {
+        city: 'Caracas',
+        country: 'Venezuela'
+    },
+    {
+        city: 'Casablanca',
+        country: 'Morocco'
+    },
+    {
+        city: 'Chicago',
+        country: 'Illinois'
+    },
+    {
+        city: 'Colombo',
+        country: 'Sri Lanka'
+    },
+    {
+        city: 'Columbus',
+        country: 'Ohio'
+    },
+    {
+        city: 'Copenhagen',
+        country: 'Denmark'
+    },
+    {
+        city: 'Dallas',
+        country: 'Texas'
+    },
+    {
+        city: 'New Delhi',
+        country: 'India'
+    },
+    {
+        city: 'Dubai',
+        country: 'United Arab Emirates'
+    },
+    {
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        city: 'Edinburgh',
+        country: 'Scotland'
+    },
+    {
+        city: 'Florence',
+        country: 'Italy'
+    },
+    {
+        city: 'Frankfurt',
+        country: 'Germany'
+    },
+    {
+        city: 'Geneva',
+        country: 'Switzerland'
+    },
+    {
+        city: 'Hong Kong',
+        country: 'Hong Kong'
+    },
+    {
+        city: 'Honolulu',
+        country: 'Hawaii'
+    },
+    {
+        city: 'Indianapolis',
+        country: 'Indiana'
+    },
+    {
+        city: 'Istanbul',
+        country: 'Turkey'
+    },
+    {
+        city: 'Jakarta',
+        country: 'Indonesia'
+    },
+    {
+        city: 'Jerusalem',
+        country: 'Israel'
+    },
+    {
+        city: 'Kathmandu',
+        country: 'Nepal'
+    },
+    {
+        city: 'Kuala Lumpur',
+        country: 'Malaysia'
+    },
+    {
+        city: 'Las Vegas',
+        country: 'Nevada'
+    },
+    {
+        city: 'Lima',
+        country: 'Peru'
+    },
+    {
+        city: 'Lisbon',
+        country: 'Portugal'
+    },
+    {
+        city: 'London',
+        country: 'England'
+    },
+    {
+        city: 'Los Angeles',
+        country: 'California'
+    },
+    {
+        city: 'Madrid',
+        country: 'Spain'
+    },
+    {
+        city: 'Male',
+        country: 'Maldives'
+    },
+    {
+        city: 'Marrakech',
+        country: 'Morocco'
+    },
+    {
+        city: 'Marseille',
+        country: 'France'
+    },
+    {
+        city: 'Mexico City',
+        country: 'Mexico'
+    },
+    {
+        city: 'Miami',
+        country: 'Florida'
+    },
+    {
+        city: 'Montego Bay',
+        country: 'Jamaica'
+    },
+    {
+        city: 'Montreal',
+        country: 'Canada'
+    },
+    {
+        city: 'Moorea',
+        country: 'Tahiti'
+    },
+    {
+        city: 'Moscow',
+        country: 'Russia'
+    },
+    {
+        city: 'Munich',
+        country: 'Germany'
+    },
+    {
+        city: 'Muscat',
+        country: 'Oman'
+    },
+    {
+        city: 'Myrtle Beach',
+        country: 'South Carolina'
+    },
+    {
+        city: 'Nairobi',
+        country: 'Kenya'
+    },
+    {
+        city: 'Nassau',
+        country: 'The Bahamas'
+    },
+    {
+        city: 'New York City',
+        country: 'United States'
+    },
+    {
+        city: 'Oslo',
+        country: 'Norway'
+    },
+    {
+        city: 'Papua',
+        country: 'New Guinea'
+    },
+    {
+        city: 'Paris',
+        country: 'France'
+    },
+    {
+        city: 'Perth',
+        country: 'Australia'
+    },
+    {
+        city: 'Phoenix',
+        country: 'Arizona'
+    },
+    {
+        city: 'Phuket',
+        country: 'Thailand'
+    },
+    {
+        city: 'Point Pleasant',
+        country: 'New Jersey'
+    },
+    {
+        city: 'Prague',
+        country: 'Czech Republic'
+    },
+    {
+        city: 'Queenstown',
+        country: 'New Zealand'
+    },
+    {
+        city: 'Reykjavik',
+        country: 'Iceland'
+    },
+    {
+        city: 'Rio De Janeiro',
+        country: 'Brazil'
+    },
+    {
+        city: 'Rome',
+        country: 'Italy'
+    },
+    {
+        city: 'Salt Lake City',
+        country: 'Utah'
+    },
+    {
+        city: 'San Francisco',
+        country: 'California'
+    },
+    {
+        city: 'Santiago',
+        country: 'Chile'
+    },
+    {
+        city: 'Santorini',
+        country: 'Greece'
+    },
+    {
+        city: 'Seoul',
+        country: 'South Korea'
+    },
+    {
+        city: 'Shanghai',
+        country: 'China'
+    },
+    {
+        city: 'Singapore',
+        country: 'Singapore'
+    },
+    {
+        city: 'Stockholm',
+        country: 'Sweden'
+    },
+    {
+        city: 'Suva',
+        country: 'Fiji'
+    },
+    {
+        city: 'Sydney',
+        country: 'Australia'
+    },
+    {
+        city: 'Taipei',
+        country: 'Taiwan'
+    },
+    {
+        city: 'Tallinn',
+        country: 'Estonia'
+    },
+    {
+        city: 'Lhasa',
+        country: 'Tibet'
+    },
+    {
+        city: 'Tokyo',
+        country: 'Japan'
+    },
+    {
+        city: 'Toronto',
+        country: 'Canada'
+    },
+    {
+        city: 'Tunis',
+        country: 'Tunisia'
+    },
+    {
+        city: 'Ulan Bator',
+        country: 'Mongolia'
+    },
+    {
+        city: 'Vancouver',
+        country: 'British Columbia'
+    },
+    {
+        city: 'Vienna',
+        country: 'Austria'
+    },
+    {
+        city: 'Washington',
+        country: 'D.C.'
+    }
+]
+
+const weatherQuiz = {};
+weatherQuiz.apiURL = 'https://api.weatherbit.io/v2.0/current';
+weatherQuiz.apikey = '651ad6758ca042e2ba4e3da504485e1d';
+
+let cityOne = 'toronto';
+let cityTwo = 'los angeles';
+
+let valueOne = 0;
+let valueTwo = 0;
+
+let answer = 0;
+
+const getWeather = function () {
+
+    $.ajax({
+        url: weatherQuiz.apiURL,
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            key: weatherQuiz.apikey,
+            city: cityOne,
+            format: 'json'
+        }
+    }).then(function (res) {
+        valueOne = (res.data[0].temp);
+    });
+
+    $.ajax({
+        url: weatherQuiz.apiURL,
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            key: weatherQuiz.apikey,
+            city: cityTwo,
+            format: 'json'
+        }
+    }).then(function (res) {
+        valueTwo = (res.data[0].temp)
+    });
+}
+
+const getCity = function () {
+    let randOne = Math.floor(Math.random() * locales.length);
+    let randTwo = Math.floor(Math.random() * locales.length);
+
+    cityOne = locales[randOne].city;
+    cityTwo = locales[randTwo].city;
+
+    $('.cityOne').html(cityOne);
+    $('.cityTwo').html(cityTwo);
+}
+
+const getAnswer = function(){
+    if (valueOne > valueTwo){
+        answer = valueOne;
+    } else {
+        answer = valueTwo;
+    }
+};
+
+const getUserAnswer =function(){
+
+};
+
+
+
 // populate first question
-// randomize 2 locations from array of popular cities
+// randomize  locations from array of popular cities
 // pull live temperature data for both randomized cities from weather API
-// populate question, generic template with names of cities plugged in
+// populate question,
+// generic template with names of cities plugged in
 // compare temperatures to determine answer
 // compare answer to user submission
 // respond with correct or incorrect styling and display current temperatures for both locations
 // populate next question
+
+quizStartcity: init = () => {
+    // on button start run init
+    getCity();
+    getUserAnswer();
+    getWeather();
+}
+
+$(function () {
+    quizStartcity: init();
+})
