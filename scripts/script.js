0dc1a4272e04a62c6052b68d50b414297980eb60
Thusa <thusa.arul@hotmail.com>
2020-06-10 14:46:40 -0400

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+// on button start run init
+// populate first question
+// randomize 2 locations from array of popular cities
+// pull live temperature data for both randomized cities from weather API
+// populate question, generic template with names of cities plugged in
+// compare temperatures to determine answer
+// compare answer to user submission
+// respond with correct or incorrect styling and display current temperatures for both locations
+// populate next question
